1d3936102aac9fd09fcfc7abe01c588ed19de0a9
ravsten <r_stencel@vp.pl>
2018-06-05 16:58:17 +0200

    class for Users

---
+++ EncryptorDecryptor1/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptorDecryptor1
+{
+    class User
+    {
+        public string Email { get; }
+        public string publicKey { get; }
+        public string privateKey { get; }
+        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+
+        public User(string Email)
+        {
+            this.Email = Email;
+            this.publicKey = rsa.ToXmlString(false);
+            this.privateKey = rsa.ToXmlString(true);
+            //RSAParameters RSAKeyInfo = rsa.ExportParameters(true);
+        }
+
+        public string encryptSessionKey(string sessionKey)
+        {
+            byte[] toEncryptData = Encoding.ASCII.GetBytes(sessionKey);
+
+            RSACryptoServiceProvider rsaPublic = new RSACryptoServiceProvider();
+            rsaPublic.FromXmlString(publicKey);
+            byte[] encryptedRSA = rsaPublic.Encrypt(toEncryptData, false);
+            string EncryptedResult = Encoding.Default.GetString(encryptedRSA);
+
+            return EncryptedResult;
+        }
+    }
+}
