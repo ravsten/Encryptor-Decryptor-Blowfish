15d2536f74c943cc6c8dc87177a1cf858131065d
ravsten <r_stencel@vp.pl>
2018-06-06 18:01:10 +0200

    encrypting private key with hashed user password; decrypting private key with hashed user password;

--- EncryptorDecryptor1/User.cs
+++ EncryptorDecryptor1/User.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EncryptorDecryptor1
 {
@@ -12,7 +12,9 @@ namespace EncryptorDecryptor1
     {
         public string Email { get; set; }
         public string publicKey { get; set; }
-        public string privateKey { get; set; }
+        private string privateKey { get; set; }
+        public string password { get; set; }
+        private byte[] encPrivateKey;
         RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
 
         public User(string Email)
@@ -20,24 +22,12 @@ namespace EncryptorDecryptor1
             this.Email = Email;
             this.publicKey = rsa.ToXmlString(false);
             this.privateKey = rsa.ToXmlString(true);
-
-            //zapisz klucze nowego uzytkownika na dysku
-            using (FileStream fs = File.Create(MainWindow.publicKeysPath + "\\" + Email + ".txt"))
-            {
-                Byte[] info = new UTF8Encoding(true).GetBytes(publicKey);
-                fs.Write(info, 0, info.Length);
-            }
-            using (FileStream fs = File.Create(MainWindow.privateKeysPath + "\\" + Email + ".txt"))
-            {
-                Byte[] info = new UTF8Encoding(true).GetBytes(privateKey);
-                fs.Write(info, 0, info.Length);
-            }
         }
 
-        public User(string Email, string publicKey)
+        public User(string Email, byte[] encPrivateKey)
         {
             this.Email = Email;
-            this.publicKey = publicKey;
+            this.encPrivateKey = encPrivateKey;
         }
 
         public string encryptSessionKey(string sessionKey)
@@ -64,6 +54,61 @@ namespace EncryptorDecryptor1
             string originalResult = Encoding.Default.GetString(decryptedRSA);
 
             return originalResult;
+        }
+
+        public string computeSha256Hash(string rawPass)
+        { 
+            using (SHA256 sha256Hash = SHA256.Create())
+            {  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawPass));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public void saveKeysToFiles()
+        {
+            //zapisz klucze nowego uzytkownika na dysku
+            using (FileStream fs = File.Create(MainWindow.publicKeysPath + "\\" + Email + ".txt"))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(publicKey);
+                fs.Write(info, 0, info.Length);
+            }
+            using (FileStream fs = File.Create(MainWindow.privateKeysPath + "\\" + Email + ".txt"))
+            {
+                Byte[] privateKeyBytes = new UTF8Encoding(true).GetBytes(privateKey);
+                string key = computeSha256Hash(password); //hashed password
+
+                BlowfishEngine engine = new BlowfishEngine();
+                PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(engine); //ECB
+                KeyParameter keyBytes = new KeyParameter(Encoding.UTF8.GetBytes(key));
+
+                cipher.Init(true, keyBytes);
+                encPrivateKey = new byte[cipher.GetOutputSize(privateKeyBytes.Length)];
+                int len1 = cipher.ProcessBytes(privateKeyBytes, 0, privateKeyBytes.Length, encPrivateKey, 0); //private key encrypted with hashed password
+                cipher.DoFinal(encPrivateKey, len1);
+
+                fs.Write(encPrivateKey, 0, encPrivateKey.Length);
+            }
+        }
+
+        public void decryptPrivateKey(string keyString)
+        {
+            BlowfishEngine engine = new BlowfishEngine();
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(engine);
+            StringBuilder result = new StringBuilder();
+
+            cipher.Init(false, new KeyParameter(Encoding.UTF8.GetBytes(keyString)));
+            byte[] out2 = new byte[cipher.GetOutputSize(encPrivateKey.Length)];
+            int len2 = cipher.ProcessBytes(encPrivateKey, 0, encPrivateKey.Length, out2, 0);
+            cipher.DoFinal(out2, len2);
+
+            this.privateKey = Encoding.UTF8.GetString(out2);
         }
     }
 }
